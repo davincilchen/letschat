2ff686cb22dfbcda705429492cba37fe0234bd3c
davincilchen <doki2099@gmail.com>
2018-06-05 21:33:37 +0800

    add socket some event

--- index.js
+++ index.js
@@ -12,21 +12,38 @@ const io = require('socket.io')(http);
 
  
 app.get('/', (req, res) => {
-  res.sendFile( __dirname + '/views/index.html');
+  res.sendFile( __dirname + '/views/index2.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
  
   socket.on("greet", function(){
-        socket.emit("greet", "Hi! Client.");
-    });
-
-  socket.on('disconnect', function(){
-    console.log('a user disconnect');
+    socket.emit("greet", "Hi! Client.");
   });
+
+
+  socket.on('add user',function(msg){
+    socket.username = msg;
+    console.log("new user: "+msg+" logged.");
+    io.emit('add user',{
+      username: socket.username
+    });
+  });
+
+  socket.on('disconnect',function(){
+    console.log('a user disconnect');
+    if(socket.username !== undefined){
+      console.log(socket.username+" left.");
+      io.emit('user left',{
+        username:socket.username
+      });
+    }
+  });
+
 });
 
-http.listen(3000, () => { //use http.listen to replace app app.listen
+http.listen(3000, () => { //use http.listen to replace app.listen
   console.log('listening on *:3000');
 });
