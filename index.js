8f89f9d84418831d995fbabcf453f8e15901f451
davincilchen <doki2099@gmail.com>
2018-06-05 17:27:43 +0800

    use socket io

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+
+const server = require('http').Server(app); //require('http').createServer(onRequest).listen(port, ip);
+const io = require('socket.io')(server);
+
  
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
+
+io.on('connection', (socket) => {
+  console.log('socket connect!');
  
+
+  socket.on('disconnect', () => {
+    console.log('socket disconnect');
+  });
+});
+
 app.listen(3000, () => {
-    console.log("Server Started. http://localhost:3000");
+  console.log("Server Started. http://localhost:3000");
 });
